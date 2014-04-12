38dba69276d7a63ce41543ba0000c92d52ef33da
Eli Gundry <eligundry@gmail.com>
2014-04-12 16:58:03 -0500

    Got a basic torrent search working

--- music_magnet/__init__.py
+++ music_magnet/__init__.py
@@ -1,6 +1,5 @@
 from flask.ext.api import FlaskAPI
 import pylast
-import discogs_client as discogs
 
 lf = pylast.LastFMNetwork()
 
@@ -13,9 +12,6 @@ def create_app(config_objects=['music_magnet.settings.site']):
     # Configure last.fm connection
     lf.api_key = app.config['LAST_FM_PUBLIC_KEY']
     lf.api_secret = app.config['LAST_FM_SECRET_KEY']
-
-    # Configure discogs user agent
-    discogs.user_agent = 'MusicMagnet/0.1 +http://music-magnet.eligundry.com'
 
     from music_magnet.views import search
 
--- music_magnet/views/search.py
+++ music_magnet/views/search.py
@@ -1,18 +1,25 @@
 import time
+import kat
 
-from music_magnet import lf, discogs
 from music_magnet.views import *
 
 blueprint = Blueprint('search', __name__)
 
 @blueprint.route('/search', methods=['GET'])
-def search_tpb():
+def search_kat():
     """
     Searches The Pirate Bay for music
     """
 
-    query = request.args.get('q')
-
     result = []
+    query = request.args.get('q')
+    torrents = kat.search(query, category=kat.Categories.MUSIC,
+            sort=kat.Sorting.SEED, order=kat.Sorting.Order.DESC)
+
+    for torrent in torrents:
+        result.append({
+            'title': torrent.title,
+            'magnet': torrent._magnet,
+        })
 
     return result
